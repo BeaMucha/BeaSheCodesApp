dd45cbc8ab130048eb44c20022aaced81569dacc
BeaMucha <168504245+BeaMucha@users.noreply.github.com>
2024-05-12 08:52:14 +0200

    added a js file

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
